7b8775f3ec755b01e4aaf232b651c120e11b7153
Your Name <louiestokk>
2021-12-26 17:12:20 +0100

    initial commit

---
+++ functions/hello.js
@@ -0,0 +1,12 @@
+// domain/.netlify/functions/hello
+
+const items = [
+  { id: 1, name: "john" },
+  { id: 2, name: "lisa" },
+];
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+};
